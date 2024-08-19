600d408455bcce8582de4e5da0169c914d358d10
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-08-19 13:45:13 -0300

    add base project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import "./global.css";
+
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
