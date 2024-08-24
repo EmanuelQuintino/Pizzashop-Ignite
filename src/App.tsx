668ed04cea003a27c78b94848942ca630d5b6b32
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-08-24 11:35:13 -0300

    add toggle theme button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { AppRoutes } from "./routes";
 import "./global.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/themeProvider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizzashop Ignite" />
-      <AppRoutes />
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
+        <Helmet titleTemplate="%s | Pizzashop Ignite" />
+        <AppRoutes />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { Home, Pizza, UtensilsCrossed } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { NavLink } from "./navLink";
+import { ThemeToggleButton } from "./theme/themeToggleButton";
 
 export function Header() {
   return (
@@ -21,6 +22,10 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggleButton />
+        </div>
       </div>
     </div>
   );
