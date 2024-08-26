1cce1b5dc3de06aa74ee44504643be539227a5f7
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-08-26 17:15:04 -0300

    add page 404

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,14 @@ import { Dashboard } from "@/pages/app/Dashboard";
 import { Orders } from "@/pages/app/Orders";
 import { SignIn } from "@/pages/auth/SignIn";
 import { SignUp } from "@/pages/auth/SignUp";
+import { Page404 } from "@/pages/page404";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Page404 />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
