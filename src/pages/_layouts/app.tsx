0fa8a163d0f7f185fee39699debc3bc87222c027
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-09-15 19:18:13 -0300

    add navigate to sign-in on error 401

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,7 +1,34 @@
 import { Header } from "@/components/header";
-import { Outlet } from "react-router-dom";
+import { API } from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export function AppLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interceptorId = API.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status;
+          const code = error.response?.data.code;
+
+          console.log({ status, code });
+
+          if (status == 401 && code == "UNAUTHORIZED") {
+            navigate("/sign-in", { replace: true });
+          }
+        }
+      },
+    );
+
+    return () => {
+      API.interceptors.response.eject(interceptorId);
+    };
+  }, [navigate]);
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
