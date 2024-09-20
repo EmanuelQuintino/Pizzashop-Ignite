4843358770d14cbe4790a60f247c85928cc0fa1f
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-09-20 13:00:49 -0300

    add tests order status

--- src/components/orderStatus.tsx
+++ src/components/orderStatus.tsx
@@ -13,7 +13,7 @@ const orderStatusMap: Record<OrderStatus, string> = {
   pending: "Pendente",
   canceled: "Cancelado",
   processing: "Em preparo",
-  delivering: "Em entraga",
+  delivering: "Em entrega",
   delivered: "Entregue",
 };
 
@@ -21,19 +21,31 @@ export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status == "pending" && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+        <span
+          className="h-2 w-2 rounded-full bg-slate-400"
+          data-testid="badge"
+        />
       )}
 
       {status == "canceled" && (
-        <span className="h-2 w-2 rounded-full bg-rose-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-rose-500"
+          data-testid="badge"
+        />
       )}
 
       {status == "delivered" && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-emerald-500"
+          data-testid="badge"
+        />
       )}
 
       {["processing", "delivering"].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+        <span
+          className="h-2 w-2 rounded-full bg-amber-500"
+          data-testid="badge"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
