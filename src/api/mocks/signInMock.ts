00aea0f9aba0d531a1ba1fa801e9ad45783fac65
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-09-24 17:36:24 -0300

    add sign in tests e2e

--- src/api/mocks/signInMock.ts
+++ src/api/mocks/signInMock.ts
@@ -14,6 +14,7 @@ export const signInMock = http.post<never, SignInProps>(
         },
       });
     }
+
     return new HttpResponse(null, { status: 401 });
   },
 );
---
+++ test/signIn.e2e.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+
+test("sign in successfully", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByLabel("Seu e-mail").fill("jonhdoe@email.com");
+  await page.getByRole("button", { name: "Acessar painel" }).click();
+
+  const toast = page.getByText(
+    "Enviamos um link de autenticação para seu email",
+  );
+
+  await expect(toast).toBeVisible();
+});
+
+test("sign in with wrong credentials", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByLabel("Seu e-mail").fill("wrong@email.com");
+  await page.getByRole("button", { name: "Acessar painel" }).click();
+
+  const toast = page.getByText("Credenciais inválidas");
+
+  await expect(toast).toBeVisible();
+});
+
+test("navigate to new restaurant page", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByRole("link", { name: "Novo estabelecimento" }).click();
+
+  expect(page.url()).toContain("/sign-up");
+});
